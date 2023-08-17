aaf7b2f39b6ddda8f863af6e3a902985b8fd169d
Samuel Rosero <saferoxx215@gmail.com>
2023-08-17 17:01:30 -0500

    fix bug with categories

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none z-40">
+      <Image
+        src={"/avatar-ia-bg.png"}
+        width={737}
+        height={678}
+        alt="Imagen de avatar personalizado"
+        className="translate-z-0 w-full h-full  "
+      />
+    </div>
+  );
 };
 
 export default Avatar;
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,28 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// fonts settings
+import { Sora } from "@next/font/google";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weights: ["100", "200,", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+//components
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImage from "../components/TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}
+    >
+      <Nav />
+      <Header />
+      {children}
+      <TopLeftImage />
+    </div>
+  );
 };
 
 export default Layout;
